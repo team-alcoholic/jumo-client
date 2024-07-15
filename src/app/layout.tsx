f38f3388e0e0a0e47d798de94be5607aa8c159d2
Youngmin Song <68019733+y-ngm-n@users.noreply.github.com>
2024-07-15 10:59:58 +0900

    [BYOB-51] 지역 선택 컴포넌트 구현 (#2)
    * feat: 지역 선택 컴포넌트 구현
    
    * feat: 지역 선택 컴포넌트에서 회원가입 페이지로 지역 정보 전달 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+          {children}
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
