6e3a796c3f67d6b8b2b7ce28f337d4a6cfc30b34
dongsuk <scienceyds@naver.com>
2024-07-16 17:18:55 +0900

    feat: 모든 이미지 사용 가능하게 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+                pathname: '/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
