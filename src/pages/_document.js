2432c18b5c10f1bb4da6b0c4e5c6b5e04822567e
yangdongsuk <51476641+yangdongsuk@users.noreply.github.com>
2024-07-16 14:24:20 +0900

    [BYOB-50, 55] OAuth 페이지, 세션 키 저장 + 로그아웃 (#4)
    * chore: mui 패키지 설치
    
    * chore: mui 적용
    
    * chore: emotion 적용
    
    * feat: 메인 페이지 구현
    
    * feat: 로그인 페이지 구현
    
    * fix: url 오류 해결
    
    * fix: package.json axios 문제 해결

---
+++ src/pages/_document.js
@@ -0,0 +1,56 @@
+// src/pages/_document.js
+import * as React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import theme from '../app/theme';
+import createEmotionCache from '../app/createEmotionCache';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <meta name="emotion-insertion-point" content="" />
+                    <meta name="theme-color" content={theme.palette.primary.main} />
+                    <link rel="shortcut icon" href="/static/favicon.ico" />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                    {this.props.emotionStyleTags}
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+    const originalRenderPage = ctx.renderPage;
+
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    ctx.renderPage = () =>
+        originalRenderPage({
+            enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
+        });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+        <style
+            data-emotion={`${style.key} ${style.ids.join(' ')}`}
+            key={style.key}
+            dangerouslySetInnerHTML={{ __html: style.css }}
+        />
+    ));
+
+    return {
+        ...initialProps,
+        emotionStyleTags,
+    };
+};
