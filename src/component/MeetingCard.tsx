c3578b73d376980c9b88a7467b47ac6fbf2eb3ec
y-ngm-n <wogitopp@naver.com>
2024-07-18 22:06:50 +0900

    feat: MeetingCard css 적용

--- src/component/MeetingCard.tsx
+++ src/component/MeetingCard.tsx
@@ -7,6 +7,12 @@ import useObserver from "@/hook/useObserver";
 import Link from "next/link";
 import useLocalStorage from "use-local-storage";
 // import { LazyLoadImage } from "react-lazy-load-image-component";
+import styles from "../style/meetings.module.css";
+
+const formatDateTime = (datetime: string) => {
+  return `${datetime.slice(5, 7)}/${datetime.slice(8, 10)}`;
+}
+
 
 export default function MeetingCard(
   { meeting }: { key: number, meeting: Meeting}
@@ -25,18 +31,25 @@ export default function MeetingCard(
     <Link
       href={`/meetings/${meeting.id}`}
       onClick={() => setScrollY(window.scrollY)}
+      className={styles.container}
     >
-      <ListItem ref={target} sx={{ minHeight: "75px" }}>
+      <ListItem ref={target} className={styles.item}>
         {visible && (
           <>
             <ListItemAvatar>
               <Avatar alt="thumbnail" src={meeting.thumbnail} />
               {/* <LazyLoadImage alt="thumbnail" src={meeting.thumbnail} width="40px" height="auto" /> */}
             </ListItemAvatar>
-            <ListItemText primary={meeting.name} secondary={`${meeting.id} ${meeting.region} ${meeting.meetingAt}`} />
+            <ListItemText
+              primary={`${meeting.id}: ${meeting.name}`}
+              secondary={`${formatDateTime(meeting.meetingAt)} ${meeting.region}`}
+            />
           </>
         )}
-      </ListItem>   
+      </ListItem>
+      <span className={styles.description}>
+        {`${meeting.liquors}, 회비 ${meeting.payment}원`}
+      </span>
     </Link>
   )
 }
