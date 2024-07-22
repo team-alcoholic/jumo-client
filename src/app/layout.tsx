bd7b39ae1daf253a1de68aa3b24b77a237c06dd3
Youngmin Song <68019733+y-ngm-n@users.noreply.github.com>
2024-07-22 16:22:28 +0900

    [BYOB-105] Google Analytics 연결 테스트 (#14)
    * feat: Google Analytics 사용을 위한 @next/third-parties 설치 및 RootLayout 수정
    
    * feat: GA ID 환경변수 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
+import { GoogleAnalytics } from "@next/third-parties/google"
 
 const inter = Inter({ subsets: ["latin"] });
+
+const gaId = process.env.NEXT_PUBLIC_GA_ID;
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,6 +25,7 @@ export default function RootLayout({
           {children}
         </Provider>
       </body>
+      { gaId && <GoogleAnalytics gaId={gaId} /> }
     </html>
   );
 }
