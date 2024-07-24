7921846f75246b47b6d4fb0ab509d7bdb42243d7
y-ngm-n <wogitopp@naver.com>
2024-07-24 22:59:49 +0900

    fix: 모임목록 페이지 type 관련 예외처리

--- src/components/MeetingCard/MeetingCard.tsx
+++ src/components/MeetingCard/MeetingCard.tsx
@@ -1,19 +1,15 @@
 "use client";
 
 import { Avatar, Box, ListItemAvatar, ListItemText } from "@mui/material";
-import { Meeting } from "../../app/meetings/page";
 import { useRef, useState } from "react";
 import useObserver from "@/hooks/useObserver";
 import useLocalStorage from "use-local-storage";
 import { DescriptionSpan, LinkButton } from "./StyledComponent";
-
-const formatDateTime = (datetime: string) => {
-  return `${datetime.slice(5, 7)}/${datetime.slice(8, 10)}`;
-}
+import { formatDate } from "@/utils/format";
 
 
 export default function MeetingCard(
-  { meeting }: { meeting: Meeting}
+  { meeting }: { meeting: MeetingInfo }
 ) {
   const [visible, setVisible] = useState(false);
   const [scrollY, setScrollY] = useLocalStorage("meeting-list-scroll", 0);
@@ -32,14 +28,18 @@ export default function MeetingCard(
             </ListItemAvatar>
             <ListItemText
               primary={`${meeting.id}: ${meeting.name}`}
-              secondary={`${formatDateTime(meeting.meetingAt)} ${meeting.region}`}
+              secondary={`${meeting.region} | ${meeting.meetingAt?.length ? formatDate(meeting.meetingAt) : "일시 미정" }`}
               primaryTypographyProps={{style:{overflow:'hidden',whiteSpace:'nowrap',textOverflow:'ellipsis'}}}
               secondaryTypographyProps={{style:{overflow:'hidden',whiteSpace:'nowrap',textOverflow:'ellipsis'}}}
             />
           </Box>
         )}
       <DescriptionSpan>
-        {visible && `${meeting.liquors}, 회비 ${meeting.payment}원`}
+        {visible && (
+          meeting.liquors || meeting.payment ? (
+            `${meeting.liquors ? meeting.liquors + ' 모임' : ''}${meeting.liquors && meeting.payment ? ', ' : ''}${meeting.payment ? `회비 ${meeting.payment}원` : ''}`
+          ) : "상세 정보를 확인해보세요."
+        )}
       </DescriptionSpan>
     </LinkButton>
   )
