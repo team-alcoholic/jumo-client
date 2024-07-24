849d963be08e9541e67778988e173b2b81095a7e
Youngmin Song <68019733+y-ngm-n@users.noreply.github.com>
2024-07-24 23:24:51 +0900

    [BYOB-108] 모임 목록 페이지에서 type 관련 리팩토링 및 예외처리 (#19)
    * feat: type들을 new-types.d.ts 파일에 리팩토링
    
    * fix: 모임목록 페이지 type 관련 예외처리
    
    * fix: type 관련 리팩토링 미적용 부분 수정

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -13,8 +13,14 @@ const days = ['일', '월', '화', '수', '목', '금', '토'];
  * 날짜를 포맷팅하는 함수
  * @param dateString
  */
-export const formatDate = (dateString: string): string => {
+export const formatDateTime = (dateString: string): string => {
     const date = new Date(dateString);
     const day = days[date.getUTCDay()];
     return `${('0' + (date.getUTCMonth() + 1)).slice(-2)}.${('0' + date.getUTCDate()).slice(-2)}(${day}) ${('0' + date.getUTCHours()).slice(-2)}:${('0' + date.getUTCMinutes()).slice(-2)}`;
+};
+
+export const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    const day = days[date.getUTCDay()];
+    return `${('0' + (date.getUTCMonth() + 1)).slice(-2)}.${('0' + date.getUTCDate()).slice(-2)}(${day})`;
 };
