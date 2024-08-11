a0a19a66b744de18a58090502670b53e94ca875e
y-ngm-n <wogitopp@naver.com>
2024-08-12 00:07:03 +0900

    feat: 주류 기본 정보 디자인 수정

--- src/app/liquors/[id]/page.tsx
+++ src/app/liquors/[id]/page.tsx
@@ -14,7 +14,6 @@ const getLiquorInfo = async (id: string) => {
     throw new Error("Failed to fetch data");
   }
   const data: LiquorData = await res.json();
-  console.log(data);
   return data;
 };
 
@@ -45,7 +44,7 @@ export default async function LiquorDetailPage({
       </Box>
 
       {/* 주류 정보 */}
-      <Stack sx={{ gap: "40px" }}>
+      <Stack sx={{ gap: "50px" }}>
         {/* 이름 */}
         <Stack>
           <Typography sx={{ color: "gray", fontSize: "10px" }}>
@@ -61,57 +60,67 @@ export default async function LiquorDetailPage({
         </Stack>
 
         {/* 정보 */}
-        <Stack sx={{ gap: "8px" }}>
-          <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>
-            주류 정보
-          </Typography>
-          <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-            <Typography sx={{ minWidth: "35px", color: "gray" }}>
-              분류
+        <Stack
+          sx={{
+            border: "solid 1px #cccccc",
+            borderRadius: "5px 5px",
+            padding: "30px",
+            gap: "50px",
+          }}
+        >
+          {/* 기본 정보 */}
+          <Stack sx={{ gap: "8px" }}>
+            <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>
+              주류 기본 정보
             </Typography>
-            <Typography>{liquor.type}</Typography>
-          </Box>
-          <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-            <Typography sx={{ minWidth: "35px", color: "gray" }}>
-              도수
-            </Typography>
-            <Typography>{liquor.abv}</Typography>
-          </Box>
-          <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-            <Typography sx={{ minWidth: "35px", color: "gray" }}>
-              국가
-            </Typography>
-            <Typography>{liquor.country}</Typography>
-          </Box>
-        </Stack>
+            <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+              <Typography sx={{ minWidth: "35px", color: "gray" }}>
+                분류
+              </Typography>
+              <Typography>{liquor.type}</Typography>
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+              <Typography sx={{ minWidth: "35px", color: "gray" }}>
+                도수
+              </Typography>
+              <Typography>{liquor.abv}</Typography>
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+              <Typography sx={{ minWidth: "35px", color: "gray" }}>
+                국가
+              </Typography>
+              <Typography>{liquor.country}</Typography>
+            </Box>
+          </Stack>
 
-        {/* 테이스팅 노트 */}
-        <Stack sx={{ gap: "8px" }}>
-          <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>
-            테이스팅 정보
-          </Typography>
-          <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-            <Typography sx={{ minWidth: "100px", color: "gray" }}>
-              향 (Aroma)
+          {/* 테이스팅 노트 */}
+          <Stack sx={{ gap: "8px" }}>
+            <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>
+              테이스팅 정보
             </Typography>
-            <Typography>{liquor.tastingNotesAroma}</Typography>
-          </Box>
-          <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-            <Typography sx={{ minWidth: "100px", color: "gray" }}>
-              맛 (Taste)
-            </Typography>
-            <Typography>{liquor.tastingNotesTaste}</Typography>
-          </Box>
-          <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-            <Typography sx={{ minWidth: "100px", color: "gray" }}>
-              여운 (Finish)
-            </Typography>
-            <Typography>{liquor.tastingNotesFinish}</Typography>
-          </Box>
+            <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+              <Typography sx={{ minWidth: "100px", color: "gray" }}>
+                향 (Aroma)
+              </Typography>
+              <Typography>{liquor.tastingNotesAroma}</Typography>
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+              <Typography sx={{ minWidth: "100px", color: "gray" }}>
+                맛 (Taste)
+              </Typography>
+              <Typography>{liquor.tastingNotesTaste}</Typography>
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+              <Typography sx={{ minWidth: "100px", color: "gray" }}>
+                여운 (Finish)
+              </Typography>
+              <Typography>{liquor.tastingNotesFinish}</Typography>
+            </Box>
+          </Stack>
         </Stack>
       </Stack>
 
-      {/* 주류 리뷰 */}
+      {/* 주류 리뷰: 컴포넌트 구현 필요 */}
       <Stack sx={{ marginTop: "50px" }}>
         <Typography sx={{ fontSize: "22px", fontWeight: "600" }}>
           커뮤니티 리뷰
