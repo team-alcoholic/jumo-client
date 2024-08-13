25edbbc63ee11f395f193ea1b94dbd05875fde5e
dongsuk <scienceyds@naver.com>
2024-08-13 11:20:29 +0900

    feat: 헤더 반응형으로 나타나게 구현

--- src/components/LayoutComponents/HeaderComponent.tsx
+++ src/components/LayoutComponents/HeaderComponent.tsx
@@ -1,12 +1,38 @@
+"use client";
 import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function HeaderComponent() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const threshold = 50; // 헤더가 나타나기 위한 스크롤 임계값
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY + threshold) {
+      setIsVisible(false); // 스크롤을 많이 내리면 헤더 숨기기
+    } else if (lastScrollY - currentScrollY > threshold) {
+      setIsVisible(true); // 스크롤을 많이 올리면 헤더 보이기
+    }
+
+    setLastScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <Box
         sx={{
           position: "fixed",
-          top: 0,
+          top: isVisible ? 0 : "-80px", // 스크롤 방향에 따라 헤더 위치 변경
           left: "50%",
           transform: "translateX(-50%)",
           width: "100%",
@@ -18,6 +44,7 @@ export default function HeaderComponent() {
           borderBottom: "1px solid #D9D9D9",
           boxShadow: "0 2px 1px -1px gray",
           zIndex: 1000,
+          transition: "top 0.3s", // 애니메이션 추가
         }}
         style={{ backgroundColor: "white" }}
       >
