1f2bb6cf54bac285a87ec0d01e78358cf177e367
yangdongsuk <51476641+yangdongsuk@users.noreply.github.com>
2024-10-30 18:24:57 +0900

    release 2.4.0 가격 비교 기능 추가 (#62)
    * feat: 레이아웃 수정 및 검색 UI 수정
    
    * feat: 홈페이지 변경
    
    * feat: 홈페이지 디자인 변경
    
    * fix: null 에러 수정
    
    * feat: 무카와 상세 설명 추가

--- src/components/LayoutComponents/NavigationComponent.tsx
+++ src/components/LayoutComponents/NavigationComponent.tsx
@@ -5,7 +5,7 @@ import {
   Diversity3,
   LocalBar,
   Warehouse,
-  EditNote,
+  Search,
 } from "@mui/icons-material";
 import { Box, Stack, Typography } from "@mui/material";
 import Link from "next/link";
@@ -61,11 +61,11 @@ export default function NavigationComponent() {
       },
     },
     {
-      title: "테이스팅노트",
+      title: "주류 가격 비교",
       link: "/liquors",
       icon: function () {
         return (
-          <EditNote
+          <Search
             sx={{
               fontSize: "20px",
               color: pathName.startsWith(this.link) ? "black" : "gray",
