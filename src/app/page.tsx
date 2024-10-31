38362aff00ffd31635528fd4c788a0c0771f1c3a
yangdongsuk <51476641+yangdongsuk@users.noreply.github.com>
2024-10-31 11:38:21 +0900

    Revert "release 2.4.0 디벨롭에 반영 (#63)"
    This reverts commit 1c55076179b9c2537063be5c218dcc1c4f397180.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+import ServiceIntroductionComponent from "@/components/ServiceIntroductionComponent/ServiceIntroductionComponent";
 import { Stack } from "@mui/material";
 import { redirect } from "next/navigation";
-import ServiceIntroductionComponentV2 from "@/components/ServiceIntroductionComponent/ServiceIntroductionComponentV2";
 
 export default function Home() {
   return (
     <Stack sx={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       {/* 서비스 소개 및 유저 피드백 관련 컴포넌트 */}
-      <ServiceIntroductionComponentV2 />
+      <ServiceIntroductionComponent />
     </Stack>
   );
   // redirect("/meetings");
